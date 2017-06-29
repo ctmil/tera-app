928aa5115fa18176ada2a91fe63093bc05f688a7
ibuioli <ibuioli@gmail.com>
2017-06-29 16:36:14 -0300

    Update Visual drag and WifiWizard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import * as Hammer from 'hammerjs';
+declare var WifiWizard:any;
 
 @Component({
   selector: 'app-root',
@@ -17,11 +18,24 @@ export class AppComponent {
     this.wW = (10*window.innerWidth)/100;
     this.wH = (10*window.innerHeight)/100;
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://192.168.1.181:8090/stream");
+    ///////////////////////
   }
 
   ngOnInit(){
+    //WiFiWizard//
+    setInterval(function(){
+      WifiWizard.getScanResults(listPrint, fail);
+    }, 100);
+
+    function listPrint(a) {
+    }
+    function fail(a){
+      alert(a);
+    }
+    //END WiFiWizard//
 
     ///////////////////////////////////////////////
+    //Definiciones de HammerJS
     let stage;
     let manager;
     let Pan;
@@ -30,8 +44,7 @@ export class AppComponent {
     let Tap;
     let DoubleTap;
 
-    let s = 'stream';
-    stage = document.getElementById(s);
+    stage = document.getElementById('stream');
 
     manager = new Hammer.Manager(stage);
 
@@ -62,7 +75,7 @@ export class AppComponent {
     //E-SCALE//
     this.eventScale(manager, stage);
 
-    ////////////////////////////////
+    ///////////////////////////////////////////////
   }
 
   eventMove(manager:any, stage:any){
@@ -72,21 +85,21 @@ export class AppComponent {
     manager.on('panmove', function(e) {
       let dX = deltaX + (e.deltaX);
       let dY = deltaY + (e.deltaY);
-      let r = stage.style.transform.match(/rotateZ\((.*deg)\)/g);
-      let s = stage.style.transform.match(/scale\((.*)\)/g);
+      let r = document.getElementById('obj').style.transform.match(/rotateZ\((.*deg)\)/g);
+      let s = document.getElementById('obj').style.transform.match(/scale\((.*)\)/g);
+      //Limitar extremos//
       if(dX > this_.wW){
-        dX = this_.wW;
-      }
-      if(dX < -this_.wW){
-        dX = -this_.wW;
+        dX = this_.wW-0.1;
+      }else if(dX < -this_.wW){
+        dX = -this_.wW+0.1;
       }
       if(dY > this_.wH){
-        dY = this_.wH;
+        dY = this_.wH-0.1;
+      }else if(dY < -this_.wH){
+        dY = -this_.wH+0.1;
       }
-      if(dY < -this_.wH){
-        dY = -this_.wH;
-      }
-      stage.style.transform = 'translate('+dX+'px, '+dY+'px) '+r+' '+s+'';
+      ///////////////////
+      document.getElementById('obj').style.transform = 'translate('+dX+'px, '+dY+'px) '+r+' '+s+'';
       document.getElementById('debug_px').innerHTML = "X: "+dX.toString();
       document.getElementById('debug_py').innerHTML = "Y: "+dY.toString();
     });
@@ -102,15 +115,17 @@ export class AppComponent {
     let currentScale = 1;
     manager.on('pinchmove', function(e) {
       let scale = e.scale * currentScale;
-      let t = stage.style.transform.match(/translate\((.*px,.*px)\)/g);
-      let r = stage.style.transform.match(/rotateZ\((.*deg)\)/g);
+      let t = document.getElementById('obj').style.transform.match(/translate\((.*px,.*px)\)/g);
+      let r = document.getElementById('obj').style.transform.match(/rotateZ\((.*deg)\)/g);
+      //Limitar Escala//
       if(scale <= 1){
         scale = 1;
       }
       if(scale >= 3){
         scale = 3;
       }
-      stage.style.transform = ''+t+' '+r+' scale('+scale+')';
+      /////////////////
+      document.getElementById('obj').style.transform = ''+t+' '+r+' scale('+scale+')';
       document.getElementById('debug_scale').innerHTML = "S: "+scale.toString();
       this_.wW = ((scale*10)*window.innerWidth)/100;
       this_.wH = ((scale*10)*window.innerHeight)/100;
