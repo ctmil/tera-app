5ef3395d62b2744763b10ae9b7ce9b0b9acd2f02
ibuioli <ibuioli@gmail.com>
2017-07-13 17:39:36 -0300

    Update Tera-App

--- cordova/platforms/android/platform_www/cordova_plugins.js
+++ cordova/platforms/android/platform_www/cordova_plugins.js
@@ -7,13 +7,39 @@ module.exports = [
         "clobbers": [
             "window.WifiWizard"
         ]
+    },
+    {
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "pluginId": "cordova-plugin-device-motion",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "id": "cordova-plugin-powermanagement.powermanagement",
+        "file": "plugins/cordova-plugin-powermanagement/www/powermanagement.js",
+        "pluginId": "cordova-plugin-powermanagement",
+        "clobbers": [
+            "window.powermanagement"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "com.pylonproducts.wifiwizard": "0.2.11",
+    "cordova-plugin-device-motion": "1.2.5",
     "cordova-plugin-whitelist": "1.3.2",
-    "com.pylonproducts.wifiwizard": "0.2.11"
+    "cordova-plugin-powermanagement": "1.0.5"
 };
 // BOTTOM OF METADATA
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
-import * as Hammer from 'hammerjs';
+//import * as Hammer from 'hammerjs';
 declare var WifiWizard:any;
+declare var navigator:any;
+declare var window:any;
 
 @Component({
   selector: 'app-root',
@@ -11,32 +13,77 @@ declare var WifiWizard:any;
 export class AppComponent {
   title = 'TeraApp';
   public url: SafeUrl;
+  public c: SafeUrl;
   public wW: any;
   public wH: any;
+  public wifiNet:any;
 
   constructor(private sanitizer: DomSanitizer){
     this.wW = (10*window.innerWidth)/100;
     this.wH = (10*window.innerHeight)/100;
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://192.168.1.181:8090/stream");
+    this.url = this.sanitizer.bypassSecurityTrustStyle('url("http://192.168.1.182:8080/stream/video.mjpeg") no-repeat center center fixed');
+    this.c = this.sanitizer.bypassSecurityTrustStyle('cover');
     ///////////////////////
   }
 
   ngOnInit(){
+    //Iniciar Dispositivo//
+    onLoad();
+
+    function onLoad() {
+        document.addEventListener("deviceready", onDeviceReady, false);
+    }
+    function onDeviceReady() {
+        window.powermanagement.acquire(); //WeakLock para que la pantalla no se bloquee
+        setInterval(function(){ startWatch(); }, 1000); //Espera un segundo antes de ver el Acc
+    }
+
     //WiFiWizard//
     setInterval(function(){
-      WifiWizard.getScanResults(listPrint, fail);
-    }, 100);
+      WifiWizard.getScanResults(listPrint, fail); //Obtiene una lista de las conexiones WiFi - EN DESARROLLO
+    }, 2000);
 
     function listPrint(a) {
+      this.wifiNet = a;
     }
     function fail(a){
       alert(a);
     }
-    //END WiFiWizard//
+
+    //Accelerometer//
+    function startWatch() {
+        var options = { frequency: 100 }; //Frecuencia del Acelerómetro, a Mayor numero más lento pero más eficiente - BUSCAR EQUILIBRIO
+        navigator.accelerometer.watchAcceleration(onAccelSuccess, onAccError, options); //Watch de la Aceleración
+    }
+
+    function onAccelSuccess(acceleration) { //Acelerómetro
+      //Calculo rápido para Rotación
+      var roll = Math.atan2(acceleration.y, acceleration.z) * 180/Math.PI;
+
+      //Para Debug - Comentar si no se usa
+      document.getElementById('acc').innerHTML =
+                          'Acceleration X: ' + acceleration.x + '<br />' +
+                          'Acceleration Y: ' + acceleration.y + '<br />' +
+                          'Acceleration Z: ' + acceleration.z + '<br />' +
+                          'Roll: '+ roll;
+
+      //Compensación de Posicion
+      document.getElementById('stream').style.top = -120+(acceleration.x*12)+"%";
+
+      if(acceleration.x > 7){
+        document.getElementById('stream').style.left = String( (-1*roll/4) - 10 )+"%";
+      }else{
+        document.getElementById('stream').style.left = "-10%";
+      }
+    }
+
+    function onAccError(e) {
+        alert('Error:'+e);
+    }
 
     ///////////////////////////////////////////////
-    //Definiciones de HammerJS
-    let stage;
+    //Definiciones de HammerJS - SIN USO actual
+    /*let stage;
     let manager;
     let Pan;
     let Rotate;
@@ -71,14 +118,14 @@ export class AppComponent {
     manager.add(Tap);
 
     //E-MOVE//
-    this.eventMove(manager, stage);
+    //this.eventMove(manager, stage);
     //E-SCALE//
-    this.eventScale(manager, stage);
+    //this.eventScale(manager, stage);*/
 
     ///////////////////////////////////////////////
   }
 
-  eventMove(manager:any, stage:any){
+  /*eventMove(manager:any, stage:any){
     let this_ = this;
     let deltaX = 0;
     let deltaY = 0;
@@ -100,8 +147,8 @@ export class AppComponent {
       }
       ///////////////////
       document.getElementById('obj').style.transform = 'translate('+dX+'px, '+dY+'px) '+r+' '+s+'';
-      document.getElementById('debug_px').innerHTML = "X: "+dX.toString();
-      document.getElementById('debug_py').innerHTML = "Y: "+dY.toString();
+      //document.getElementById('debug_px').innerHTML = "X: "+dX.toString();
+      //document.getElementById('debug_py').innerHTML = "Y: "+dY.toString();
     });
     manager.on('panend', function(e) {
         deltaX = deltaX + e.deltaX;
@@ -121,12 +168,12 @@ export class AppComponent {
       if(scale <= 1){
         scale = 1;
       }
-      if(scale >= 3){
-        scale = 3;
+      if(scale >= 1.5){
+        scale = 1.5;
       }
       /////////////////
       document.getElementById('obj').style.transform = ''+t+' '+r+' scale('+scale+')';
-      document.getElementById('debug_scale').innerHTML = "S: "+scale.toString();
+      //document.getElementById('debug_scale').innerHTML = "S: "+scale.toString();
       this_.wW = ((scale*10)*window.innerWidth)/100;
       this_.wH = ((scale*10)*window.innerHeight)/100;
     });
@@ -134,5 +181,5 @@ export class AppComponent {
       currentScale = e.scale * currentScale;
       liveScale = currentScale;
     });
-  }
+  }*/
 }
